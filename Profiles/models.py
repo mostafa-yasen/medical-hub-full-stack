64b0b1a6735029042c3c4ab2cc8794a029e4978f
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2019-06-14 10:51:16 +0200

    Inhireted Django's User Model and constructed our 'Profile' model.

--- Profiles/models.py
+++ Profiles/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+GENDERS = (
+    ('M', 'male'),
+    ('F', 'female')
+)
+
+class Profile(models.Model):
+    # Extend Django Auth's User
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Start Global Attributes
+    pic = models.ImageField(upload_to='images/', default='images/no-pic.png')
+
+    bio = models.TextField(max_length=500, blank=True)
+    
+    city = models.CharField(max_length=30, blank=True)
+    
+    address = models.CharField(max_length=120, blank=True)
+    
+    gender = models.CharField(max_length=1, choices=GENDERS, default='m')
+    
+    is_doctor = models.BooleanField(default=False)
+    
+    nid = models.CharField(max_length=15, blank=True)
+
+    # Start Patient Attributes
+
+    # Start Doctor Attributes
+    specialization = models.CharField(max_length=120, blank=True)
+
+    rate = models.FloatField(default=0)
+
+    # Start Methods
+    def __str__(self):
+        if self.is_doctor:
+            return 'Dr.%s' % self.user.username
+        
+        else:
+            return str(self.user.username)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
